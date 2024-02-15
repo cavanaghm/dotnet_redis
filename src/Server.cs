f79c900c0dc00e62f4d19975cea2d7b59ac77087
Michael Cavanagh <mcavanagh28@gmail.com>
2024-02-15 15:59:28 +1000

    accept connection

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 server.AcceptSocket(); // wait for client
+
+// Accept incoming connection
+TcpClient client = server.AcceptTcpClient();
+NetworkStream stream = client.GetStream();
+byte[] buffer = new byte[client.ReceiveBufferSize];
+int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+Console.WriteLine(dataReceived);
